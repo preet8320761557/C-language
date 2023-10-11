14b4c75f68eb6ff85a37b42b4b302262c0d4b7cf
preet8320761557 <147600642+preet8320761557@users.noreply.github.com>
2023-10-11 15:15:27 +0530

    C day-1

---
+++ print.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+  printf("My name is  BHALALA PREET\n");
+    printf("I am 18 year old\n");
+    printf("I am study Bsc cs-it in silver oak univesity form ahmedabad\n");
+
+    printf("\nTHANK YOU");
+
+
+
+    return 0;
+}
---
+++ project-2.c
@@ -0,0 +1,602 @@
+
+1.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+    printf("Name:\tPREET BHALALA\n");
+    printf("Age:\t18\n");
+    printf("School:\tShree Vasistha Vidhyalay");
+    
+
+    return 0;
+}
+
+----------------------------------------------
+2.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+printf("-\t\t-\t\t-\t\t-\t\t-\n");
+printf("|\t\t\t\t\t\t\t\t|\n");
+printf("|\t\t\t\t\t\t\t\t|\n");
+printf("|\t\t\tPREET\t\t\t\t|\n");
+printf("|\t\t\t\t\t\t\t\t|\n");
+printf("|\t\t\t\t\t\t\t\t|\n");
+printf("-\t\t-\t\t-\t\t-\t\t-\n");
+
+    return 0;
+}
+
+--------------------------------------
+3.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+
+printf("_\t\t_\t\t_\n");
+printf("|\t\t\t\t|\n");
+printf("P\t\t\t\t|\n");
+printf("R\t\t\t\t|\n");
+printf("E\t\t\t\t|\n");
+printf("E\t\t\t\t|\n");
+printf("T\t\t\t\t|\n");
+printf("|\t\t\t\t|\n"); 
+printf("_\t\t_\t\t_\n"); 
+
+  
+
+    return 0;
+}
+
+
+--------------------------------------
+4.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    printf("*\t\n");
+    printf("*\t*\t\n");
+    printf("*\t*\t*\t\n");
+    printf("*\t*\t\n");
+     printf("*\t\n");
+    
+
+    return 0;
+}
+
+
+-----------------------------------
+5.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    printf("*\t\n");
+    printf("*\t\n");
+    printf("*\t\n");
+    printf("*\t\n");
+    printf("*\t\t\t\t*\t*\t\t\t\t\t\t\t*\t*\n");
+    printf("*\t\t\t*\t\t\t*\t\t\t\t\t*\t\t\t*\n");
+    printf("*\t\t*\t\t\t\t\t*\t\t\t*\n");
+    printf("*\t*\t\t\t\t\t\t\t*\t*\n");
+    printf("*\t\n");
+    
+    
+
+    return 0;
+}
+
+
+-------------------------------------------------
+6.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+   printf("*\t*\t*\t*\t*\n");
+   printf("*\t\t\t\t*\n");
+   printf("*\t*\t*\t*\t*\n");
+   printf("*\t\t\t\t*\n");
+   printf("*\t\t\t\t*\n");
+   printf("*\t\t\t\t*\n");
+   printf("*\t\t\t\t*\n");
+
+    return 0;
+}
+
+-------------------------------------------------
+7.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+   printf("*\t*\t*\n");
+   printf("*\t\t\t*\n");
+   printf("*\t\t\t*\n");
+   printf("*\t*\t*\n");
+   printf("*\t\t\t*\n");
+   printf("*\t\t\t*\n");
+   printf("*\t*\t*\n");
+
+    return 0;
+}
+
+----------------------------------------------
+8.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+   printf("*\t*\t*\t*\n");
+   printf("*\t\n");
+   printf("*\t\n");
+   printf("*\t\n");
+   printf("*\t\n");
+   printf("*\t\n");
+   printf("*\t*\t*\t*\n");
+
+    return 0;
+}
+
+----------------------------------------------
+9.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+   printf("*\t*\n");
+   printf("*\t\t*\n");
+   printf("*\t\t*\n");
+   printf("*\t\t*\n");
+   printf("*\t*\n");
+   
+  
+    return 0;
+}
+
+--------------------------------------------
+10.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+   printf("*\t*\t*\t*\n");
+   printf("*\t\n");
+   printf("*\t\n");
+   printf("*\t*\t*\t*\n");
+   printf("*\t\n");
+   printf("*\t\n");
+   printf("*\t*\t*\t*\n");
+  
+   
+  
+    return 0;
+}
+
+----------------------------------------------
+11.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+   printf("*\t*\t*\t*\n");
+   printf("*\t\n");
+   printf("*\t\n");
+   printf("*\t*\t*\t*\n");
+   printf("*\t\n");
+   printf("*\t\n");
+   printf("*\t\n");
+  
+   
+  
+    return 0;
+}
+
+-------------------------------------------
+12.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+   printf("*\t*\t*\n");
+   printf("*\t\t\t*\n");
+   printf("*\n");
+   printf("*\t\t***\n");
+   printf("*\t\t\t*\n");
+   printf("*\t\t\t*\n");
+   printf("*\t*\t*\n");
+  
+  
+   
+  
+    return 0;
+}
+
+-------------------------------------------
+13.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+   printf("*\t\t\t*\n");
+   printf("*\t\t\t*\n");
+   printf("*\t\t\t*\n");
+   printf("*\t*\t*\t*\n");
+   printf("*\t\t\t*\n");
+   printf("*\t\t\t*\n");
+   printf("*\t\t\t*\n");
+
+  
+    return 0;
+}
+
+---------------------------------------------
+14.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+   printf("*\t*\t*\t*\t*\n");
+   printf("\t\t*\n");
+   printf("\t\t*\n");
+   printf("\t\t*\n");
+   printf("\t\t*\n");
+   printf("\t\t*\n");
+   printf("*\t*\t*\t*\t*\n");
+
+  
+    return 0;
+}
+
+--------------------------------------------
+15.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+   printf("*\t*\t*\t*\t*\n");
+   printf("\t\t*\n");
+   printf("\t\t*\n");
+   printf("\t\t*\n");
+   printf("\t\t*\n");
+   printf("*\t\t*\n");
+   printf("*\t*\n");
+
+  
+    return 0;
+}
+
+--------------------------------------------
+16.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+   printf("*\t\t\t\t*\n");
+   printf("*\t\t\t*\n");
+   printf("*\t\t*\n");
+   printf("*\t*\n");
+   printf("*\t\t*\n");
+   printf("*\t\t\t*\n");
+   printf("*\t\t\t\t*\n");
+
+  
+    return 0;
+}
+
+---------------------------------------
+17.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+   printf("*\t\n");
+   printf("*\t\n");
+   printf("*\t\n");
+   printf("*\t\n");
+   printf("*\t\n");
+   printf("*\t\n");
+   printf("*\t*\t*\t*\n");
+
+  
+    return 0;
+}
+
+------------------------------------------
+18.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+   printf("*\t\t\t\t*\n");
+   printf("*\t*\t\t*\t*\n");
+   printf("*\t\t*\t\t*\n");
+   printf("*\t\t\t\t*\n");
+   printf("*\t\t\t\t*\n");
+   printf("*\t\t\t\t*\n");
+   printf("*\t\t\t\t*\n");
+
+  
+    return 0;
+}
+
+-------------------------------------------
+19.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+   printf("*\t\t\t\t*\n");
+   printf("*\t\t\t\t*\n");
+   printf("*\t*\t\t\t*\n");
+   printf("*\t\t*\t\t*\n");
+   printf("*\t\t\t*\t*\n");
+   printf("*\t\t\t\t*\n");
+   printf("*\t\t\t\t*\n");
+  
+    return 0;
+}
+
+-----------------------------------------
+20.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+    printf("\t*\t*\t*\n");
+    printf("*\t\t\t\t*\n");
+    printf("*\t\t\t\t*\n");
+    printf("*\t\t\t\t*\n");
+    printf("*\t\t\t\t*\n");
+    printf("*\t\t\t\t*\n");
+    printf("\t*\t*\t*\n");     
+  
+    return 0;
+}
+
+------------------------------------------
+21.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+   printf("*\t*\t*\n");
+   printf("*\t\t\t*\n");
+   printf("*\t\t\t*\n");
+   printf("*\t*\t*\n");
+   printf("*\t\n");
+   printf("*\t\n");
+   printf("*\t\n");     
+  
+    return 0;
+}
+
+--------------------------------------------
+22.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+printf("\t*\t*\t*\n");
+printf("*\t\t\t\t*\n");
+printf("*\t\t\t\t*\n");
+printf("*\t\t\t\t*\n");
+printf("*\t\t\t\t*\n");
+printf("*\t\t*\t\t*\n");
+printf("\t*\t*\t*\n");
+printf("\t\t\t\t*\n");     
+  
+    return 0;
+}
+
+-------------------------------------------
+23.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+   printf("*\t*\t*\n");
+   printf("*\t\t\t*\n");
+   printf("*\t\t\t*\n");
+   printf("*\t*\t*\n");
+   printf("*\t*\n");
+   printf("*\t\t*\n");
+   printf("*\t\t\t*\n");    
+  
+    return 0;
+}
+
+------------------------------------------
+24.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+   printf("*\t*\t*\t*\n");
+   printf("*\t\n");
+   printf("*\t\n");
+   printf("\t*\t*\t*\t\n");
+   printf("\t\t\t*\n");
+   printf("\t\t\t*\n");
+   printf("*\t*\t*\t*\n");    
+  
+    return 0;
+}
+
+-----------------------------------------
+25.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+   printf("*\t*\t*\t*\t*\n");
+   printf("\t\t*\n");
+   printf("\t\t*\n");
+   printf("\t\t*\n");
+   printf("\t\t*\n");
+   printf("\t\t*\n");
+   printf("\t\t*\n");
+    return 0;
+}
+
+----------------------------------------------
+26.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+    printf("*\t\t\t\t*\n");
+    printf("*\t\t\t\t*\n");
+    printf("*\t\t\t\t*\n");
+    printf("*\t\t\t\t*\n");
+    printf("*\t\t\t\t*\n");
+    printf("*\t\t\t\t*\n");
+    printf("\t*\t*\t*\n"); 
+    return 0;
+}
+
+------------------------------------------------
+27.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+    printf("*\t\t\t\t*\n");
+    printf("*\t\t\t\t*\n");
+    printf("*\t\t\t\t*\n");
+    printf("*\t\t\t\t*\n");
+    printf("\t*\t\t*\t\n");
+    printf("\t\t*\t\t\n");
+    
+    return 0;
+}
+
+---------------------------------------------
+28.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+    printf("*\t\t\t\t*\n");
+    printf("*\t\t\t\t*\n");
+    printf("*\t\t\t\t*\n");
+    printf("*\t\t\t\t*\n");
+    printf("*\t\t*\t\t*\n");
+    printf("*\t*\t\t*\t*\n");
+    printf("*\t\t\t\t*\n");
+    
+    return 0;
+}
+--------------------------------------------
+29.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+    printf("*\t\t\t\t\t\t*\n");
+    printf("\t*\t\t\t\t*\n");
+    printf("\t\t*\t\t*\n");
+    printf("\t\t\t*\n");
+    printf("\t\t*\t\t*\n");
+    printf("\t*\t\t\t\t*\n");
+    printf("*\t\t\t\t\t\t*\n");
+
+    
+    return 0;
+}
+---------------------------------------------
+30.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+   printf("*\t\t\t\t\t\t*\n");
+   printf("\t*\t\t\t\t*\n");
+   printf("\t\t*\t\t*\n");
+   printf("\t\t\t*\n");
+   printf("\t\t\t*\n");
+   printf("\t\t\t*\n");
+   printf("\t\t\t*\n");
+
+    
+    return 0;
+}
+---------------------------------------------
+31.
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+    printf("*\t*\t*\t*\t*\t\n");
+    printf("\t\t\t\t\t*\n");
+    printf("\t\t\t\t*\n");
+    printf("\t\t\t*\n");
+    printf("\t\t*\n");
+    printf("\t*\n");
+    printf("*\t*\t*\t*\t*\n");
+    
+    return 0;
+}
