f1c6d2bc01105d4b70f968d860f4ff5712280d9d
preet8320761557 <147600642+preet8320761557@users.noreply.github.com>
2023-10-11 15:25:12 +0530

    C day-2

---
+++ scanf.c
@@ -0,0 +1,55 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main()
+ {
+     int a;
+     printf("enter a vaue:");
+     scanf("%d",&a);
+     printf("A: %d",a);
+      
+
+    return 0;
+}
+--------------------------------------------
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main()
+ {
+     char a;
+     printf("enter a value:");
+     scanf("%c",&a);
+     printf("A: %c",a);
+      
+
+    return 0;
+}
+-------------------------------------------
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main()
+ {
+     float a;
+     printf("enter a vaue:");
+     scanf("%f",&a);
+     printf("enter a value: %f",a);
+      
+
+    return 0;
+}
+--------------------------------------------------
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main()
+ {
+     double a;
+     printf("enter a vaue:");
+     scanf("%lf",&a);
+     printf("enter a value: %lf",a);
+      
+
+    return 0;
+}
